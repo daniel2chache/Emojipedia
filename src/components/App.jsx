4714e6f0e6437db598bc663cd18d99c891299508
Daniel Chache <dmchache@gmail.com>
2020-11-17 08:31:01 +0300

    2nd commit

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+//1. the props
+//icon
+//name
+//description
+
+//2 Map through emojipidea const
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
